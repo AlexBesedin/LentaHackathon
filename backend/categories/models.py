ca9963c29ed9ec48100e8aac3cb1b80ddc7eda8e
Виктория Карина <victoria.m.karina@mail.ru>
2023-09-25 14:19:49 +0300

    model category_v2

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,4 +1,56 @@
+from categories.models import Category
 from django.db import models
+
+
+class Group(models.Model):
+    """Класс Группа продуктовой иерархии."""
+
+    group = models.CharField(
+        max_length=50,
+        verbose_name='захэшированная группа товара',
+    )
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+        ordering = ("group",)
+
+    def __str__(self):
+        return self.group
+
+
+class CateroryProduct(models.Model):
+    """Класс Категория продуктовой иерархии."""
+
+    category = models.CharField(
+        max_length=50,
+        verbose_name='захэшированная категория товара',
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ("category",)
+
+    def __str__(self):
+        return self.category
+
+
+class Subcategory(models.Model):
+    """Класс субкатегорий продуктовой иерархии."""
+
+    subcategory = models.CharField(
+        max_length=50,
+        verbose_name='захэшированная подкатегория товара',
+    )
+
+    class Meta:
+        verbose_name = "Субкатегория"
+        verbose_name_plural = "Субкатегории"
+        ordering = ("category",)
+
+    def __str__(self):
+        return self.category
 
 
 class Category(models.Model):
@@ -8,17 +60,20 @@ class Category(models.Model):
         max_length=50,
         verbose_name='захэшированное id товара',
     )
-    group = models.CharField(
-        max_length=50,
-        verbose_name='захэшированная группа товара',
+    group = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='groups'
     )
-    category = models.CharField(
-        max_length=50,
-        verbose_name='захэшированная категория товара',
+    category = models.ForeignKey(
+        CateroryProduct,
+        on_delete=models.CASCADE,
+        related_name='categories'
     )
-    subcategory = models.CharField(
-        max_length=50,
-        verbose_name='захэшированная подкатегория товара',
+    subcategory = models.ForeignKey(
+        Subcategory,
+        on_delete=models.CASCADE,
+        related_name='subcategories'
     )
     uom = models.PositiveIntegerField(
         verbose_name='маркер, обозначающий продаётся товар на вес или в ШТ',
