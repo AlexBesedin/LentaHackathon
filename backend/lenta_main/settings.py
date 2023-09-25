1f61c90f5e99ffa38153beb9351f320d3474ab6d
Alex Besedin <alebesedin@yandex.ru>
2023-09-25 16:22:03 +0300

    add swagger/redoc

--- backend/lenta_main/settings.py
+++ backend/lenta_main/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'forecast',
     'users',
     # 'celery',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- backend/lenta_main/urls.py
+++ backend/lenta_main/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from rest_framework import permissions
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Предсказание временных рядом",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/', include('api.v_1.urls')),
     path('admin/', admin.site.urls),
     
