b346cbb273fd2cc8c6cd72b36adb9d5450205bd3
Виктория Карина <victoria.m.karina@mail.ru>
2023-09-25 14:21:49 +0300

    correct categ_v2

--- backend/api/v_1/tasks.py
+++ backend/api/v_1/tasks.py
@@ -1,10 +1,10 @@
-# from celery import shared_task
-# from django.core.management import call_command
+from celery import shared_task
+from django.core.management import call_command
 
 
-# @shared_task
-# def import_stores_data():
-#     """Функция для обработки команды загрузки данных
-#     магазинов из файла в БД."""
+@shared_task
+def import_stores_data():
+    """Функция для обработки команды загрузки данных
+    магазинов из файла в БД."""
 
-#     call_command('import_stores')
+    call_command('import_stores')
