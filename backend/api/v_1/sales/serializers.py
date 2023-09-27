07e7621ace31232daf71acb5d2772ae4e7190cea
Alex Besedin <alebesedin@yandex.ru>
2023-09-27 10:41:11 +0300

    Добавлен сериализатор для записи и метод create во вью

--- backend/api/v_1/sales/serializers.py
+++ backend/api/v_1/sales/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from sales.models import SalesRecord, Sales
 
 class SalesRecordSerializer(serializers.ModelSerializer):
+    """Сериализато для чтения исторических данных продаж"""
     
     class Meta:
         model = SalesRecord
@@ -15,9 +16,48 @@ class SalesRecordSerializer(serializers.ModelSerializer):
             ]
         
 
+class SalesRecordWriteSerializer(serializers.ModelSerializer):
+    """Сериализатор для входящих исторических данных продаж
+    на запись в бд"""
+    class Meta:
+        model = SalesRecord
+        fields = [
+            'date',
+            'sales_type',
+            'sales_units',
+            'sales_units_promo',
+            'sales_rub',
+            'sales_rub_promo',
+        ]        
+        
+
 class SalesSerializer(serializers.ModelSerializer):
-    fact = SalesRecordSerializer(source='sales_records', many=True, read_only=True)
+    """Сериализатор продаж"""
+    fact = SalesRecordSerializer(
+        source='sales_records', 
+        many=True, 
+        read_only=True
+    )
+    sales_records = SalesRecordWriteSerializer(
+        many=True, 
+        write_only=True, 
+        required=False
+    )
     
     class Meta:
         model = Sales
-        fields = ['store', 'sku', 'fact']
+        fields = [
+            'store', 
+            'sku', 
+            'fact', 
+            'sales_records'
+        ]
+        
+    def create(self, validated_data):
+        sales_records_data = validated_data.pop('sales_records', [])
+        sales = Sales.objects.create(**validated_data)
+        for sales_record_data in sales_records_data:
+            SalesRecord.objects.create(fact=sales, **sales_record_data)
+        return sales
+        
+        
--- backend/api/v_1/sales/views.py
+++ backend/api/v_1/sales/views.py
@@ -1,16 +1,29 @@
-from rest_framework import permissions, viewsets, response
+from rest_framework import permissions, viewsets, response, status
 from sales.models import Sales
 from api.v_1.sales.serializers import SalesSerializer
-from rest_framework.decorators import action
 
 
-@action(detail=True, methods=['get'])
 class SalesViewSet(viewsets.ModelViewSet):
     """Viewset для объектов модели Sales"""
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Sales.objects.all()
     serializer_class = SalesSerializer
     permission_classes = [permissions.IsAdminUser]
+    http_method_names = ['get', 'post']
+    
+    def create(self, request, *args, **kwargs):
+        """
+        Переопределенный метод для POST запроса на создание объекта Sales
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return response.Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED, 
+            headers=headers
+        )
+    
     
     def list(self, request, *args, **kwargs):
         """
