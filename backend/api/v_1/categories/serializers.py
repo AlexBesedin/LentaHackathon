b9b7b9aa29b674d4527df7abdadb5b6a930ee5c8
Alex Besedin <alebesedin@yandex.ru>
2023-09-28 16:10:34 +0300

    fix

--- backend/api/v_1/categories/serializers.py
+++ backend/api/v_1/categories/serializers.py
@@ -1,12 +1,12 @@
-from categories.models import Category
+from categories.models import Category, CategoryProduct, Group, Subcategory
 from rest_framework import serializers
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    group = serializers.StringRelatedField()
-    category = serializers.StringRelatedField()
-    subcategory = serializers.StringRelatedField()
-    
+    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all())
+    category = serializers.PrimaryKeyRelatedField(queryset=CategoryProduct.objects.all())
+    subcategory = serializers.PrimaryKeyRelatedField(queryset=Subcategory.objects.all())
+
     class Meta:
         model = Category
         fields = (
@@ -16,3 +16,10 @@ class CategorySerializer(serializers.ModelSerializer):
             'subcategory',
             'uom',
         )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['group'] = instance.group.group
+        representation['category'] = instance.category.category
+        representation['subcategory'] = instance.subcategory.subcategory
+        return representation
