5fa583f8fcc516dd37842a3ab9b12dba48837a2c
Alex Besedin <alebesedin@yandex.ru>
2023-09-28 11:13:23 +0300

    add reset password, change password

--- backend/api/v_1/users/serializers.py
+++ backend/api/v_1/users/serializers.py
@@ -1,6 +1,7 @@
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 from users.models import CustomUser
+from django.contrib.auth import authenticate
 
 
 class MyUserCreateSerializer(UserCreateSerializer):
@@ -15,14 +16,44 @@ class MyUserCreateSerializer(UserCreateSerializer):
             'password'
             )
         extra_kwargs = {'password': {'write_only': True}}
-        
+               
 
-class SetSuperuserSerializer(serializers.Serializer):
-    """Сериализатор отображения полей для назначениия
-    суперюзера"""
-    id = serializers.IntegerField()
-    username = serializers.CharField()
+class PasswordChangeSerializer(serializers.Serializer):
+    """Сериализатор изменения пароля"""
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not authenticate(username=user.username, password=value):
+            raise serializers.ValidationError('Старый пароль неверен.')
+        return value
+
+    def save(self):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
     
-    class Meta:
-        model = CustomUser
-      
+
+class PasswordResetSerializer(serializers.Serializer):
+    """Сериализатор для сброса пароля"""
+    username = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_username(self, value):
+        try:
+            user = CustomUser.objects.get(username=value)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError(
+                'Пользователь с таким именем пользователя не существует.'
+            )
+        return value
+
+    def save(self):
+        username = self.validated_data['username']
+        user = CustomUser.objects.get(username=username)
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
+
