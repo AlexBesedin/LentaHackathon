a7d470b2679352c29f798e9f543d3a1a0f567dce
Виктория Карина <victoria.m.karina@mail.ru>
2023-09-28 16:49:32 +0300

    filter_stores

--- backend/api/v_1/stores/filters.py
+++ backend/api/v_1/stores/filters.py
@@ -5,11 +5,11 @@ from stores.models import Store
 class StoreFilter(django_filters.FilterSet):
     store = django_filters.CharFilter(field_name="store")
     city = django_filters.CharFilter(field_name="city")
-    division = django_filters.CharFilter(field_name="division")
-    type_format = django_filters.NumberFilter(field_name="type_format__id")
-    loc = django_filters.NumberFilter(field_name="loc__loc")
-    size = django_filters.NumberFilter(field_name="size__size")
-    is_active = django_filters.BooleanFilter(field_name="is_active__is_active")
+    division = django_filters.CharFilter(field_name="division__division")
+    type_format = django_filters.NumberFilter(field_name="type_format")
+    loc = django_filters.NumberFilter(field_name="loc")
+    size = django_filters.NumberFilter(field_name="size")
+    is_active = django_filters.BooleanFilter(field_name="is_active")
 
     class Meta:
         model = Store
