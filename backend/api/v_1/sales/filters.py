cc10bd1098810def867823e1414d81640d06af74
Alex Besedin <alebesedin@yandex.ru>
2023-10-02 19:42:57 +0300

    fix sales filters

--- backend/api/v_1/sales/filters.py
+++ backend/api/v_1/sales/filters.py
@@ -6,6 +6,8 @@ class SalesFilter(filters.FilterSet):
     store = filters.CharFilter(field_name="store__store")
     sku = filters.CharFilter(field_name="sku__sku")
     date = filters.DateFilter(field_name="fact__date")
+    date_from = filters.DateFilter(field_name="fact__date", lookup_expr='gte')
+    date_to = filters.DateFilter(field_name="fact__date", lookup_expr='lte')
     sales_type = filters.NumberFilter(field_name="fact__sales_type")
     sales_units = filters.NumberFilter(field_name="fact__sales_units")
     sales_units_promo = filters.NumberFilter(field_name="fact__sales_units_promo")
@@ -18,9 +20,11 @@ class SalesFilter(filters.FilterSet):
             'store', 
             'sku', 
             'date', 
+            'date_from',
+            'date_to',
             'sales_type', 
             'sales_units', 
             'sales_units_promo', 
             'sales_rub', 
             'sales_run_promo'
-            ]
+        ]
