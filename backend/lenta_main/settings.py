22fc756a0a26b621e8fbd762a7b1c47957e15ff5
Alex Besedin <alebesedin@yandex.ru>
2023-10-05 00:31:49 +0300

    fix pagination

--- backend/lenta_main/settings.py
+++ backend/lenta_main/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',],
-    "DEFAULT_PAGINATION_CLASS": "api.v_1.utils.pagination.PageLimitPagination",
+    "DEFAULT_PAGINATION_CLASS": "api.v_1.utils.pagination.CustomPageNumberPagination",
     'PAGE_SIZE': 6,
 }
 
