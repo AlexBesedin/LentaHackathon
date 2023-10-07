ba3532bf326079d02df7a24d696d3d26e5b85c9c
Alex Besedin <alebesedin@yandex.ru>
2023-10-08 01:40:51 +0300

    ml service done

--- backend/ml/model.py
+++ backend/ml/model.py
@@ -12,23 +12,22 @@ def forecast(sales: dict, item_info: dict, store_info: dict) -> list:
     :params store_info: характеристики магазина
 
     """
-    sales = pd.DataFrame([sales])
-    item_info = pd.DataFrame([item_info])
-    store_info = pd.DataFrame([store_info])
+    sales = pd.DataFrame(sales)
+    item_info = pd.DataFrame(item_info, index = [0])
+    store_info = pd.DataFrame(store_info, index = [0])
 
     a = store_info['store'].values[0]
     b = item_info['sku'].values[0]
-    c = sales['sales_type']
+    c = sales['sales_type'].values[0]
     sales['store'] = a
     sales['sku'] = b
 
-    forecast_dates = [date.today() + timedelta(days=d) for d in range(1, 14)]
+    forecast_dates = [date.today() + timedelta(days=d) for d in range(1, 15)]
     forecast_dates = [el.strftime("%Y-%m-%d") for el in forecast_dates]
     forecast_dates = pd.DataFrame(forecast_dates, columns=['date'])
-    forecast_dates['st_id'] = a
-    forecast_dates['pr_sku_id'] = b
-    forecast_dates['pr_sales_type_id'] = c
-    #Правки
+    forecast_dates['store'] = a
+    forecast_dates['sku'] = b
+    forecast_dates['pr_sales_type_id'] = int(c)
     forecast_dates['date'] = pd.to_datetime(forecast_dates['date'])
     forecast_dates['dayofweek'] = forecast_dates['date'].dt.dayofweek
 
@@ -60,8 +59,8 @@ def forecast(sales: dict, item_info: dict, store_info: dict) -> list:
                           'dayofweek': 'dayofweek'
                           }, inplace=True)
 
-    forecast_dates.rename(columns={'pr_sku_id': 'pr_sku_id',
-                          'st_id': 'st_id',
+    forecast_dates.rename(columns={'sku': 'pr_sku_id',
+                          'store': 'st_id',
                           'date': 'date',
                           'city': 'st_city_id',
                           'division': 'st_division_id',
@@ -77,98 +76,119 @@ def forecast(sales: dict, item_info: dict, store_info: dict) -> list:
 
                           }, inplace=True)
 
-    product_labels = pd.read_csv('ml/products_labels.csv')
+    product_labels = pd.read_csv('products_labels.csv')
     product_labels = product_labels[['product_clasters', 'pr_sku_id']]
     forecast_dates = pd.merge(forecast_dates, product_labels, on="pr_sku_id")
 
-    price_labels = pd.read_csv('ml/price_labels.csv')
+    price_labels = pd.read_csv('price_labels.csv')
     price_labels = price_labels[['price_clasters', 'pr_sku_id']]
     forecast_dates = pd.merge(forecast_dates, price_labels, on="pr_sku_id")
-
-    prophet_m = joblib.load('ml/prophet_m.pkl')
-    future = prophet_m.make_future_dataframe(periods=14, freq='D')
+    prophet_m = joblib.load('proph.pkl')
+    future = prophet_m.make_future_dataframe(periods=100, freq='D')
     forecast = prophet_m.predict(future)
     forecast['date'] = forecast['ds']
     forecast = forecast[['trend', 'date', 'trend_upper', 'trend_lower', 'yhat']]
     forecast_dates = pd.merge(forecast_dates, forecast, on='date')
-
-    holidays_df = pd.read_csv('ml/holidays_covid_calendar.csv')
+    holidays_df = pd.read_csv('holidays_covid_calendar.csv')
     list_col = ['date', 'holiday']
+    holidays_df['date'] = pd.to_datetime(holidays_df['date'], format = '%d.%m.%Y')
     forecast_dates = forecast_dates.merge(holidays_df[list_col], on="date", how='inner')
     forecast_dates = forecast_dates.drop('st_is_active', axis=1)
-    date_1 = -1
-    rol7, rol14, pr_sales_median, pr_sales_max, pr_sales_min, pr_sales_var = [], [], [], [], [], []
-    lag_7, lag_14, lag_1, lag_2, lag_3, lag_4, lag_5, lag_6, lag_8, lag_9, lag_10, lag_11, lag_12, lag_13 =\
-        [], [], [], [], [], [], [], [], [], [], [], [], [], []
 
-    for i in range(1, 15):
-        a = sales['date'].iloc[date_1 - 15:date_1]
-        b = sales[sales['date'].isin(a)]['pr_sales_in_units']
-        rol = b.shift().rolling(window=7).mean()
-        rol = rol.iloc[-1]
-        rol7.append(b)
+    units = sales['pr_sales_in_units']
 
-        rol = b.shift().rolling(window=14).mean()
-        rol = rol.iloc[-1]
-        rol14.append(b)
+    forecast_dates['rol7'] = 0
+    forecast_dates['rol14'] = 0
+    forecast_dates['pr_sales_min'] = 0
+    forecast_dates['pr_sales_max'] = 0
+    forecast_dates['pr_sales_median'] = 0
+    forecast_dates['pr_sales_variance'] = 0
+    forecast_dates['lag_1'] = 0
+    forecast_dates['lag_2'] = 0
+    forecast_dates['lag_3'] = 0
+    forecast_dates['lag_4'] = 0
+    forecast_dates['lag_5'] = 0
+    forecast_dates['lag_6'] = 0
+    forecast_dates['lag_7'] = 0
+    forecast_dates['lag_8'] = 0
+    forecast_dates['lag_9'] = 0
+    forecast_dates['lag_10'] = 0
+    forecast_dates['lag_11'] = 0
+    forecast_dates['lag_12'] = 0
+    forecast_dates['lag_13'] = 0
+    forecast_dates['lag_14'] = 0
 
-        rol = b.shift().rolling(window=14).min()
-        rol = rol.iloc[-1]
-        pr_sales_min.append(b)
+    model_price = joblib.load('model_price_2.pkl')
+    model_units = joblib.load('model_cb-2.pkl')
 
-        rol = b.shift().rolling(window=14).max()
-        rol = rol.iloc[-1]
-        pr_sales_max.append(b)
+    result = []
+    for index, row in forecast_dates.iterrows():
+        row['date'] = 1
+        row_price = row[['st_id', 'pr_sku_id', 'date', 'pr_group_id',
+                                    'pr_cat_id', 'pr_sales_type_id', 'pr_subcat_id',
+                                    'pr_uom_id', 'st_city_id', 'st_division_id',
+                                    'st_type_format_id', 'st_type_loc_id',
+                                    'st_type_size_id', 'product_clasters',
+                                    'price_clasters', 'holiday', 'dayofweek'
 
-        rol = b.shift().rolling(window=14).median()
-        rol = rol.iloc[-1]
-        pr_sales_median.append(b)
+                                    ]]
 
-        rol = b.shift().rolling(window=14).var()
-        rol = rol.iloc[-1]
-        pr_sales_var.append(b)
+        row['price'] = model_price.predict(row_price)
 
-        lag_1.append(b.iloc[-2])
-        lag_2.append(b.iloc[-3])
-        lag_3.append(b.iloc[-4])
-        lag_4.append(b.iloc[-5])
-        lag_5.append(b.iloc[-6])
-        lag_6.append(b.iloc[-7])
-        lag_7.append(b.iloc[-8])
-        lag_8.append(b.iloc[-9])
-        lag_9.append(b.iloc[-10])
-        lag_10.append(b.iloc[-11])
-        lag_11.append(b.iloc[-12])
-        lag_12.append(b.iloc[-13])
-        lag_13.append(b.iloc[-14])
-        lag_14.append(b.iloc[-15])
+        try:
+            rol7 = units.shift().rolling(7).mean()
+            row['rol7'] = rol7.iloc[-1]
+        except:
+            pass
 
-        date_1 -= 1
+        try:
+            rol14 = units.shift().rolling(14).mean()
+            row['rol14'] = rol14.iloc[-1]
+        except:
+            row['rol14'] = 0
 
-    forecast_dates['rol7'] = rol7
-    forecast_dates['rol14'] = rol14
-    forecast_dates['pr_sales_min'] = pr_sales_min
-    forecast_dates['pr_sales_max'] = pr_sales_max
-    forecast_dates['pr_sales_median'] = pr_sales_median
-    forecast_dates['pr_sales_variance'] = pr_sales_var
-    forecast_dates['lag_1'] = lag_1
-    forecast_dates['lag_2'] = lag_2
-    forecast_dates['lag_3'] = lag_3
-    forecast_dates['lag_4'] = lag_4
-    forecast_dates['lag_5'] = lag_5
-    forecast_dates['lag_6'] = lag_6
-    forecast_dates['lag_7'] = lag_7
-    forecast_dates['lag_8'] = lag_8
-    forecast_dates['lag_9'] = lag_9
-    forecast_dates['lag_10'] = lag_10
-    forecast_dates['lag_11'] = lag_11
-    forecast_dates['lag_12'] = lag_12
-    forecast_dates['lag_13'] = lag_13
-    forecast_dates['lag_14'] = lag_14
+        try:
+            row['pr_sales_min'] = units.shift().rolling(14).min()
+            row['pr_sales_max'] = units.shift().rolling(14).max()
+            row['pr_sales_median'] = units.shift().rolling(14).median()
+            row['pr_sales_variance'] = units.shift().rolling(14).var()
+        except:
+            pass
 
-    model = joblib.load('ml/catboost.plk')
-    predictions = model.predict(forecast_dates)
+        for lag in range(1, 15):
+            try:
+                row['lag_{}'.format(lag)] = units.iloc[-lag]
+            except:
+                continue
 
-    return predictions
+        row = row.fillna(0)
+
+        row = row[['st_id', 'pr_sku_id', 'date', 'pr_sales_type_id', 'pr_group_id',
+       'pr_cat_id', 'pr_subcat_id', 'pr_uom_id', 'st_city_id',
+       'st_division_id', 'st_type_format_id', 'st_type_loc_id',
+       'st_type_size_id', 'price', 'rol7', 'rol14', 'pr_sales_min',
+       'pr_sales_max', 'pr_sales_median', 'pr_sales_variance', 'lag_1',
+       'lag_2', 'lag_3', 'lag_4', 'lag_5', 'lag_6', 'lag_7', 'lag_8', 'lag_9',
+       'lag_10', 'lag_11', 'lag_12', 'lag_13', 'lag_14', 'dayofweek',
+       'product_clasters', 'price_clasters', 'trend', 'trend_upper',
+       'trend_lower', 'yhat', 'holiday']]
+
+        row['pr_sales_max'] = 0
+        row['pr_sales_min'] = 0
+        row['pr_sales_median'] = 0
+        row['pr_sales_variance'] = 0
+
+        pred = model_units.predict(row)
 
 
+        result.append(pred)
+
+
+    forecast_dates = forecast_dates.fillna(0)
+    forecast_dates['rol14'] = forecast_dates['rol14'].astype('float')
+    forecast_dates['rol7'] = forecast_dates['rol7'].astype('float')
+    forecast_dates['dayofweek'] = forecast_dates['dayofweek'].astype('int')
+    forecast_dates['product_clasters'] = forecast_dates['product_clasters'].astype('int')
+    forecast_dates['price_clasters'] = forecast_dates['price_clasters'].astype('int')
+
+    return result
